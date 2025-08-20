ba497d3b4485bc4e0d3b4eb474d709073908a226
kauemarini <kauemarini@gmail.com>
2025-08-20 00:46:36 -0300

    Adicionar arquivos de projeto.

---
+++ Prova/DAL.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace Prova
+{
+    class DAL
+    {
+        private static String strConexao = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=BDFarinha.mdb";
+        private static OleDbConnection conn = new OleDbConnection(strConexao);
+        private static OleDbCommand strSQL;
+        private static OleDbDataReader result;
+
+        public static void conecta()
+        {
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception)
+            {
+                Erro.setMsg("Problemas ao se conectar ao Banco de Dados");
+            }
+
+        }
+
+        public static void desconecta()
+        {
+            conn.Close();
+        }
+
+        public static bool consultaUmCliente(string cnpj)
+        {
+            try
+            {
+                string sql = "SELECT * FROM Clientes WHERE CNPJ = @cnpj";
+                strSQL = new OleDbCommand(sql, conn);
+                strSQL.Parameters.AddWithValue("@cnpj", cnpj);
+                result = strSQL.ExecuteReader();
+
+                if (result.Read())
+                {
+                    Cliente.setCNPJ(result["CNPJ"].ToString());
+                    Cliente.setNome(result["Nome"].ToString());
+                    result.Close();
+                    return true;
+                }
+                result.Close();
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Erro.setMsg("Erro ao consultar cliente: " + ex.Message);
+                return false;
+            }
+        }
+
+        public static List<string[]> consultaVendasCliente(string cnpj)
+        {
+            List<string[]> vendas = new List<string[]>();
+            try
+            {
+                string sql = "SELECT * FROM VendasCliente WHERE CNPJ = @cnpj";
+                strSQL = new OleDbCommand(sql, conn);
+                strSQL.Parameters.AddWithValue("@cnpj", cnpj);
+                result = strSQL.ExecuteReader();
+
+                while (result.Read())
+                {
+                    string data = Convert.ToDateTime(result["DataVenda"]).ToShortDateString();
+                    string toneladas = result["Toneladas"].ToString();
+                    string valor = result["Valor"].ToString();
+
+                    vendas.Add(new string[] { data, toneladas, valor });
+                }
+                result.Close();
+            }
+            catch (Exception ex)
+            {
+                Erro.setMsg("Erro ao consultar vendas: " + ex.Message);
+            }
+            return vendas;
+        }
+
+
+    }
+}
---
+++ Prova/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prova
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BLL.conecta();
+            if (Erro.getErro())
+                MessageBox.Show(Erro.getMsg());
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            BLL.desconecta();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cliente.setCNPJ(textBox1.Text.Trim());
+
+            BLL.validaCNPJ();
+            if (Erro.getErro())
+            {
+                MessageBox.Show(Erro.getMsg());
+                return;
+            }
+
+            if (!BLL.getCliente(Cliente.getCNPJ()))
+            {
+                MessageBox.Show("Cliente não encontrado!");
+                return;
+            }
+
+            textBox2.Text = Cliente.getNome();
+
+            var vendas = BLL.getVendasCliente(Cliente.getCNPJ());
+
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+
+            decimal totalTon = 0;
+            decimal totalVal = 0;
+
+            foreach (var venda in vendas)
+            {
+                listBox1.Items.Add(venda[0]); // Data
+                listBox2.Items.Add(venda[1]); // Toneladas
+                listBox3.Items.Add(venda[2]); // Valor
+
+                decimal ton;
+                if (decimal.TryParse(venda[1], out ton))
+                    totalTon += ton;
+
+                decimal val;
+                if (decimal.TryParse(venda[2], out val))
+                    totalVal += val;
+            }
+
+            textBox3.Text = totalTon.ToString("N2");
+            textBox4.Text = totalVal.ToString("C");
+        }
+    }
+}
